5369fe25b4143ae65b3483e5ef0543663de4505e
ueki18 <32954520+ueki18@users.noreply.github.com>
2026-07-07 16:37:29 +0900

    Add files via upload

---
+++ Chapter03/03-06_gamma_correction.py
@@ -0,0 +1,15 @@
+import cv2
+import numpy as np
+
+img = cv2.imread("input.jpg", cv2.IMREAD_GRAYSCALE)
+
+gamma = 1.5
+# 高速化のために「ルックアップテーブル(LUT)」を作成
+invGamma = 1.0 / gamma
+table = np.array([((i / 255.0) ** invGamma) * 255
+                  for i in np.arange(0, 256)]).astype("uint8")
+
+# LUTを用いて画像全体を一括変換
+img_gamma = cv2.LUT(img, table)
+
+cv2.imwrite("gamma.png", img_gamma)
