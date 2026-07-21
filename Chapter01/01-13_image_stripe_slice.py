401f00b89999dee3fbcbbe2872934c6f4f3b6c0e
ueki18 <32954520+ueki18@users.noreply.github.com>
2026-07-22 07:15:12 +0900

    Update 01-13_image_stripe_slice.py

--- Chapter01/01-13_image_stripe_slice.py
+++ Chapter01/01-13_image_stripe_slice.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 
-width, height = 256, 192
+height, width = 192, 256
 img = np.zeros((height, width, 3), np.uint8)
 img[0:64, :]   = [255, 0, 0]    # 上1/3を青に
 img[64:128, :] = [0, 255, 0]    # 中1/3を緑に
