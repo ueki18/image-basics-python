1f68d2d68f3902e28a3a952373e61cca2bfab702
ueki18 <32954520+ueki18@users.noreply.github.com>
2026-07-31 16:07:09 +0900

    Update 11-07_hue_shift.py

--- Chapter11/11-07_hue_shift.py
+++ Chapter11/11-07_hue_shift.py
@@ -1,12 +1,14 @@
 import cv2
+import numpy as np
 
 image = cv2.imread("cityscape.png")
 hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+hue = hsv[:, :, 0].astype(np.int32)  # 元のHueをint32で保持
 
 hue_shift = 0
 
 while True:
-    hsv[:, :, 0] = (hsv[:, :, 0] + hue_shift) % 180
+    hsv[:, :, 0] = ((hue + hue_shift) % 180).astype(np.uint8)
     hue_shift = (hue_shift + 1) % 180
 
     shifted_image = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
